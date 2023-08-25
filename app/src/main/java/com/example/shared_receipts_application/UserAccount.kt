9b1d7734cdcc4f5305904c5888d25c3989d512c5
sunhee kim <iris_starry_@naver.com>
2023-08-25 15:31:20 +0900

    ✨ 로그인, 회원가입 기능 구현

---
+++ app/src/main/java/com/example/shared_receipts_application/UserAccount.kt
@@ -0,0 +1,11 @@
+package com.example.shared_receipts_application
+
+class UserAccount {
+    var idToken: String? = null
+
+    var emailId: String? = null
+
+    var password: String? = null
+}
+
+
