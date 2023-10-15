5aaa68d57a99ab54516be003075cb88df0fbad20
Jinseon <128007743+dlwlstjs@users.noreply.github.com>
2023-10-15 12:17:48 +0900

    writting2

--- app/src/main/java/com/example/shared_receipts_application/WrittingScript2Activity.kt
+++ app/src/main/java/com/example/shared_receipts_application/WrittingScript2Activity.kt
@@ -3,9 +3,9 @@ package com.example.shared_receipts_application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class WrittingScriptActivity : AppCompatActivity() {
+class WrittingScript2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_writting_script)
+        setContentView(R.layout.activity_writting_script2)
     }
 }
