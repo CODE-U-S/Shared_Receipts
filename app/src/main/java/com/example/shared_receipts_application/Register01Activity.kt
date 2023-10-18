974ccfc91f01f849156ecc126c6cc59e5b72abbd
iris-starry <iris_starry_@naver.com>
2023-10-18 09:34:13 +0900

    :lipstick: 회원가입 UI 완성

--- app/src/main/java/com/example/shared_receipts_application/Register01Activity.kt
+++ app/src/main/java/com/example/shared_receipts_application/Register01Activity.kt
@@ -1,8 +1,11 @@
 package com.example.shared_receipts_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.CheckBox
+import android.widget.ImageButton
 
 class Register01Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,23 +18,40 @@ class Register01Activity : AppCompatActivity() {
         val checkbox03 = findViewById<CheckBox>(R.id.checkbox_03)
 
         checkboxAll.setOnClickListener {
-            // Checkbox All이 클릭되었을 때 수행할 동작
             val isChecked = checkboxAll.isChecked
             checkbox01.isChecked = isChecked
             checkbox02.isChecked = isChecked
             checkbox03.isChecked = isChecked
         }
 
+        val loginButton = findViewById<ImageButton>(R.id.register01_loginbutton)
+        loginButton.setOnClickListener {
+            if (checkbox01.isChecked && checkbox02.isChecked) {
+                // Register02Activity로 화면 전환
+                val intent = Intent(this, Register02Activity::class.java)
+                startActivity(intent)
+            } else {
+                // checkbox_01와 checkbox_02 중 하나 이상이 체크되지 않았을 때 아무 동작 없음
+            }
+        }
+
+        // checkbox_01, checkbox_02, checkbox_03 클릭 이벤트 처리
         checkbox01.setOnClickListener {
-            // Checkbox 01이 클릭되었을 때 수행할 동작
+            if (checkboxAll.isChecked) {
+                checkboxAll.isChecked = false
+            }
         }
 
         checkbox02.setOnClickListener {
-            // Checkbox 02가 클릭되었을 때 수행할 동작
+            if (checkboxAll.isChecked) {
+                checkboxAll.isChecked = false
+            }
         }
 
         checkbox03.setOnClickListener {
-            // Checkbox 03이 클릭되었을 때 수행할 동작
+            if (checkboxAll.isChecked) {
+                checkboxAll.isChecked = false
+            }
         }
     }
 }
