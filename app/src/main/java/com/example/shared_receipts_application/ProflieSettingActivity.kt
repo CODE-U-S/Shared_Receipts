36fb4f23e9f906661841530af33339719e1a37ae
Jinseon <128007743+dlwlstjs@users.noreply.github.com>
2023-10-25 10:48:41 +0900

    ✨ 마이페이지 화면 전환(로그아웃, 회원탈퇴)

--- app/src/main/java/com/example/shared_receipts_application/ProflieSettingActivity.kt
+++ app/src/main/java/com/example/shared_receipts_application/ProflieSettingActivity.kt
@@ -1,11 +1,21 @@
 package com.example.shared_receipts_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class ProflieSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_proflie_setting)
+
+        val settingButton = findViewById<Button>(R.id.settingcheckbutton)
+        settingButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/shared_receipts_application/WrittingScript1Activity.kt
+++ app/src/main/java/com/example/shared_receipts_application/WrittingScript1Activity.kt
@@ -1,12 +1,20 @@
 package com.example.shared_receipts_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WrittingScript1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writting_script1)
-        val items = arrayOf("여행","ㅁㅇㄹㄴㄹ","ㅁㄴㄹ","ㅁ","ㄴㅇ")
+
+        val writting2Button = findViewById<Button>(R.id.writting1_nextbutton)
+        writting2Button.setOnClickListener {
+            val myIntent = Intent(this, WrittingScript2Activity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 }
